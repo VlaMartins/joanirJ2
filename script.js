ee59627e7a5dc8b55738074db0e3f647f532bf2e
VladismaraMartins <100156223+VlaMartins@users.noreply.github.com>
2025-06-24 16:42:47 -0300

    22

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function inserirNome(){ //nome da função
    let nomeUsuario = prompt("Qual seu nome?"); 
    //nome da variavel, o sinal de = (igual) representa que a partir de agora ele vai receber outros valores prompt (cria um quadro de texto na tela com msg)
-   let elemento = document.querySelector("#nome-usuario"); //
+   let elemento = document.querySelector("#nome-usuario"); //estou dizendo pra variavel armazenar uma palavra. Vai la no html e encontre e selecione algo específico, no caso selecione aquele que tem o id chamado nome-usuario
    elemento.textContent = nomeUsuario;
